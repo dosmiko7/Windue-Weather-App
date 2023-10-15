4d56a74b34f2196458c2488f7aa92c60ac631da1
dosmiko7 <mikolaj.oberda@gmail.com>
2023-10-15 14:37:05 +0200

    fix: changed data for SearchesList in Map window

--- src/windows/Geolocation.jsx
+++ src/windows/Geolocation.jsx
@@ -1,21 +1,19 @@
+import { useContext } from "react";
+import { WeatherContext } from "../context/WeatherContext";
+
 import OutletLayout from "../ui/OutletLayout";
 import Map from "../features/Map/Map";
 import Sidebar from "../ui/Sidebar";
 import LatestSearchesList from "../features/LatestSearches/LatestSearchesList";
 
-// TODO: Change data from dump to API's
-const LATEST_SEARCHES = [
-	{ city: "Madrid", condition: "sunny", time: "10:23", temp: "31" },
-	{ city: "Malaga", condition: "cloudy", time: "11:23", temp: "33" },
-	{ city: "Malacky", condition: "windy", time: "11:23", temp: "22" },
-];
-
 const Geolocation = () => {
+	const { searchHistory } = useContext(WeatherContext);
+
 	return (
 		<OutletLayout>
 			<Map />
 			<Sidebar variant="nonColor">
-				<LatestSearchesList searches={LATEST_SEARCHES} />
+				<LatestSearchesList searches={searchHistory} />
 			</Sidebar>
 		</OutletLayout>
 	);
